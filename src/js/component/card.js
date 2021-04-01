aeda9904698aac1029a804847c9c236fcc6cdb7a
JimmyFV <jfletesv@hotmail.com>
2021-04-01 18:58:09 +0000

    componente card creado y background de vista home

---
+++ src/js/component/card.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export const Card = props => {
+	return (
+		<div className="card">
+			<img
+				src="https://smoda.elpais.com/wp-content/uploads/2019/12/2-look-750x702.jpg"
+				className="card-img-top"
+				alt="..."
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{props.var1}</h5>
+				<p className="card-text">{props.var2}</p>
+				<p className="card-text">{props.var3}</p>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	var1: PropTypes.string,
+	var2: PropTypes.string,
+	var3: PropTypes.strings
+};
--- src/js/views/people.js
+++ src/js/views/people.js
@@ -1,7 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Card } from "../component/card";
+
 export const People = () => {
 	const { store, actions } = useContext(Context);
-	// console.log(store.people);
-	return <h1>Hello People</h1>;
+	console.log(store.people);
+	return (
+		<div className="container">
+			<div className="card-deck">
+				{store.people.map((item, index) => {
+					return (
+						<div key={index} className="col-md-4">
+							<Card var1={item.name} var2={item.hair_color} var3={item.gender} />
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
